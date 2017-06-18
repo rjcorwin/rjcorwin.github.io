c316423cd98654e539d1560a96921c109de886c8
Robert Steinert <rjsteinert@Roberts-MacBook-Air.local>
2017-06-18 02:41:50 -0400

    Fathers day

---
+++ fathers-day/ny-times/The New York Times - Breaking News, World News & Multimedia_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "WINOOSKI",
+      'continent': "NA",
+      'country': "US",
+      'region': "VT"
+    },
+    'ip':"75.68.172.62"
+  }]);
+})
+//
+()
+
+;
